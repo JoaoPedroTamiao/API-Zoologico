f504bbb254f3fea4f40beb1e23306a44ab02d04a
Lucas Hideki Miyazaki <lucasmiyazaki6@gmail.com>
2024-02-02 15:51:04 -0300

    cadartro de Ave feito

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,55 @@
-import cors from "cors";
 import express from "express";
+import cors from "cors";
+import { Ave } from "./model/Ave";
+import { Reptil } from "./model/Reptil";
+import { Mamifero } from "./model/Mamifero";
 
-const port: number = 3000;
-
+const port: number = 3001;
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-server.get('/teste', (req, res) => {
-    res.json('Olá mundo!!');
-})
+server.get('/teste1', (req, res) => {
+    let ave: Ave = new Ave(`Papagaio`, 30, `Masculino`, 12);
+    res.json(ave);
+});
+
+server.get('/teste2', (req, res) => {
+    let reptil: Reptil = new Reptil(`Jaraqué`, 15, `Masculino`, `escamosa`);
+    res.json(reptil);
+});
+
+server.get('/teste3', (req, res) => {
+    let mamifero: Mamifero = new Mamifero(`Jubileu`, 26, `Masculino`, `gente/homi`);
+    res.json(mamifero);
+});
+
+server.post('/cadastro', (req, res) => {
+    try {
+        const { nome, idade, genero, envergadura } = req.body;
+        const ave = new Ave(nome, idade, genero, envergadura);
+
+        console.log('Ave cadastrada:', ave);
+        persistirAve(ave);
+
+        res.json({ mensagem: "Ave cadastrada com sucesso", ave });
+    } catch (error) {
+        console.error('Erro ao cadastrar ave:', error);
+        res.status(500).json({ mensagem: "Erro ao cadastrar ave" });
+    }
+});
+
+let bancoDadosAve: Ave[] = [];
+function persistirAve(ave: Ave) {
+    try {
+        bancoDadosAve.push(ave);
+        console.log(`Ave persistida:`, ave);
+    } catch (error) {
+        console.error(`Erro ao persistir os dados\n ${error}`);
+    }
+}
 
 server.listen(port, () => {
-    console.log(`Servidor está esutando no endereço http://localhost:${port}`);
-})
+    console.log(`Servidor está escutando no endereço http://localhost:${port}`);
+});
--- src/model/Reptil.ts
+++ src/model/Reptil.ts
@@ -1,7 +1,8 @@
-class Reptil extends Animal{
+import { Animal } from "./Animal";
+export class Reptil extends Animal{
     private tipo_de_escamas: string;
 
-    constructor(_tipo_de_escamas: string, _nome: string, _idade: number, _genero: string) {
+    constructor(_nome: string, _idade: number, _genero: string, _tipo_de_escamas: string) {
         super(_nome, _idade, _genero);
         this.tipo_de_escamas = _tipo_de_escamas;
     }
