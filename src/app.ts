c41205b4643682523483280df341d2fd5c092d98
Lucas Hideki Miyazaki <lucasmiyazaki6@gmail.com>
2024-02-02 16:49:48 -0300

    Correção

--- src/app.ts
+++ src/app.ts
@@ -10,28 +10,30 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.get('/teste1', (req, res) => {
+// Rotas para testes individuais
+server.get('/testeAve', (req, res) => {
     let ave: Ave = new Ave(`Papagaio`, 30, `Masculino`, 12);
     res.json(ave);
 });
 
-server.get('/teste2', (req, res) => {
+server.get('/testeReptil', (req, res) => {
     let reptil: Reptil = new Reptil(`Jaraqué`, 15, `Masculino`, `escamosa`);
     res.json(reptil);
 });
 
-server.get('/teste3', (req, res) => {
+server.get('/testeMamifero', (req, res) => {
     let mamifero: Mamifero = new Mamifero(`Jubileu`, 26, `Masculino`, `gente/homi`);
     res.json(mamifero);
 });
 
-server.post('/cadastro', (req, res) => {
+// Rotas para cadastro
+server.post('/cadastroAve', (req, res) => {
     try {
         const { nome, idade, genero, envergadura } = req.body;
         const ave = new Ave(nome, idade, genero, envergadura);
 
         console.log('Ave cadastrada:', ave);
-        persistirAve(ave);
+        persistirAnimal(ave);
 
         res.json({ mensagem: "Ave cadastrada com sucesso", ave });
     } catch (error) {
@@ -40,16 +42,59 @@ server.post('/cadastro', (req, res) => {
     }
 });
 
-let bancoDadosAve: Ave[] = [];
-function persistirAve(ave: Ave) {
+server.post('/cadastroReptil', (req, res) => {
     try {
-        bancoDadosAve.push(ave);
-        console.log(`Ave persistida:`, ave);
-    } catch (error) {
-        console.error(`Erro ao persistir os dados\n ${error}`);
-    }
-}
+        const { nome, idade, genero, tipo_de_escamas } = req.body;
+        const reptil = new Reptil(nome, idade, genero, tipo_de_escamas);
 
+        console.log('Reptil cadastrado:', reptil);
+        persistirAnimal(reptil);
+
+        res.json({ mensagem: "Reptil cadastrado com sucesso", reptil });
+    } catch (error) {
+        console.error('Erro ao cadastrar reptil:', error);
+        res.status(500).json({ mensagem: "Erro ao cadastrar reptil" });
+    }
+});
+
+server.post('/cadastroMamifero', (req, res) => {
+    try {
+        const { nome, idade, genero, raca } = req.body;
+        const mamifero = new Mamifero(nome, idade, genero, raca);
+
+        console.log('Mamifero cadastrado:', mamifero);
+        persistirAnimal(mamifero);
+
+        res.json({ mensagem: "Mamifero cadastrado com sucesso", mamifero });
+    } catch (error) {
+        console.error('Erro ao cadastrar Mamifero:', error);
+        res.status(500).json({ mensagem: "Erro ao cadastrar mamifero" });
+    }
+});
+
+server.get('/ListarAnimais', (req, res) => {
+    try {
+        const listaDeAnimais = listarAnimais();
+        console.log(`Retornando a lista de animais cadastrados`);
+        res.json(listaDeAnimais);
+    } catch (error) {
+        console.error('Erro ao listar animais:', error);
+        res.status(500).json({ mensagem: "Erro ao obter a lista de animais" });
+    }
+});
+
+function listarAnimais() {
+    const listaAves = bancoDadosAnimais.filter(animal => animal instanceof Ave);
+    const listaRepteis = bancoDadosAnimais.filter(animal => animal instanceof Reptil);
+    const listaMamiferos = bancoDadosAnimais.filter(animal => animal instanceof Mamifero);
+
+    return {
+        todasAsAves: listaAves,
+        todosOsRepteis: listaRepteis,
+        todosOsMamiferos: listaMamiferos,
+        todosOsAnimais: bancoDadosAnimais
+    };
+}
 server.listen(port, () => {
     console.log(`Servidor está escutando no endereço http://localhost:${port}`);
 });
---
+++ src/model/Atracao.ts
@@ -0,0 +1,28 @@
+import { Animal } from "./Animal";
+export class Atracao {
+  private nome: string;
+  private lista_de_Animais: Animal;
+
+  constructor(_nome: string, _lista_de_Animais: Animal) {
+    this.nome = _nome;
+    this.lista_de_Animais = _lista_de_Animais;
+  }
+
+  public getNome(): string {
+    return this.nome;
+  }
+
+  public setNome(_nome: string): void {
+    this.nome = _nome;
+  }
+
+  public getLista_de_Animais(): Animal {
+    return this.lista_de_Animais;
+  }
+
+  public setLista_de_Animais(_lista_de_Animais: Animal): void {
+    this.lista_de_Animais = _lista_de_Animais;
+  }
+
+  
+}
